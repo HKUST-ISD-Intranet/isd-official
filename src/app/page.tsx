b02c65921a534581ac0290f928bee4a82768f4d4
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-05 00:55:08 +0800

    feat(layout): add header, footer and navbar to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Styles from './styles/page';
-
 export default function Home() {
-    return (
-        <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-base-100">
-            <Styles />
-        </div>
-    );
+    return <div className="container"></div>;
 }
