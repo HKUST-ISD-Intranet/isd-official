f683a45320786f45b7613ba2513197c781084a03
Candbot Chan <candbot@outlook.com>
2025-10-06 03:04:59 +0800

    feat: added hero image

---
+++ src/components/blocks/HeroCarouselBlock.tsx
@@ -0,0 +1,23 @@
+import Carousel1 from '@/assets/carousel-1.png';
+import Image from 'next/image';
+
+export default function HeroCarouselBlock() {
+    return (
+        <div className="w-full relative overflow-hidden rounded-lg">
+            <div className="absolute -z-1 w-full h-full">
+                <Image
+                    src={Carousel1}
+                    alt="Carousel Image 1"
+                    className="object-cover w-full h-full"
+                />
+            </div>
+            <div className="absolute -z-1 w-full h-full bg-gradient-to-b via-transparent to-black"></div>
+            <div className="container text-white flex flex-col justify-end py-[64px] h-[612px]">
+                <h1 className="text-h1 text-[36px]">Welcome to</h1>
+                <h1 className="text-h1">
+                    Division of Integrative Systems and Design
+                </h1>
+            </div>
+        </div>
+    );
+}
