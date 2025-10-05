c75ab3423f9fbadf3a831842cc25beb0e38ecba3
Candbot Chan <candbot@outlook.com>
2025-10-06 03:16:04 +0800

    feat: added dot grid icon(?) to the hero image

--- src/components/blocks/HeroCarouselBlock.tsx
+++ src/components/blocks/HeroCarouselBlock.tsx
@@ -1,3 +1,4 @@
+import DotPattern from '@/assets/hero-dot-pattern.svg';
 import Carousel1 from '@/assets/carousel-1.png';
 import Image from 'next/image';
 
@@ -13,10 +14,15 @@ export default function HeroCarouselBlock() {
             </div>
             <div className="absolute -z-1 w-full h-full bg-gradient-to-b via-transparent to-black"></div>
             <div className="container text-white flex flex-col justify-end py-[64px] h-[612px]">
-                <h1 className="text-h1 text-[36px]">Welcome to</h1>
-                <h1 className="text-h1">
-                    Division of Integrative Systems and Design
-                </h1>
+                <div className="flex justify-between items-center">
+                    <div className="flex flex-col">
+                        <h1 className="text-h1 text-[36px]">Welcome to</h1>
+                        <h1 className="text-h1">
+                            Division of Integrative Systems and Design
+                        </h1>
+                    </div>
+                    <Image src={DotPattern} alt="Dot Pattern" />
+                </div>
             </div>
         </div>
     );
