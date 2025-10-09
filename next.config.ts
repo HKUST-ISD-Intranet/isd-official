0b9998d836a9bfa589cf30c71eedf8185bd55b25
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-09 20:21:45 +0800

    fix(svg): add config to handle svgs

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,13 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true, // Disables image optimization for static export
     },
+    webpack(config) {
+        config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
