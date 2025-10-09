20553b87d1884c0df34a373ffb990ecce20331d9
Candbot Chan <candbot@outlook.com>
2025-10-09 22:15:33 +0800

    fix: well well well

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export', // Enables static export
-    basePath: process.env.CI === 'true' ? '/isd-official' : '', 
-    assetPrefix: process.env.CI === 'true' ? '/isd-official/' : '', 
+    basePath: process.env.CI === 'true' ? '/isd-official' : '',
+    assetPrefix: process.env.CI === 'true' ? '/isd-official/' : '',
     trailingSlash: true,
     images: {
         unoptimized: true, // Disables image optimization for static export
     },
     webpack(config) {
         config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
         });
         return config;
     },
