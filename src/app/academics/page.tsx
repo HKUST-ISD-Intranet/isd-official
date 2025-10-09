5f148e133b8f43331109bdc1784e56e1fc502704
Candbot Chan <candbot@outlook.com>
2025-10-09 22:15:59 +0800

    feat: HeroImage can be used for any image

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -1,3 +1,5 @@
+import HeroImageFile from '@/assets/academics-hero.jpg';
+
 import HeroImage from '@/components/HeroImage';
 import ProgramBlock from '@/components/blocks/ProgramsBlock';
 import ResourceBlock from '@/components/blocks/ResourceBlock';
@@ -5,7 +7,7 @@ import ResourceBlock from '@/components/blocks/ResourceBlock';
 export default function AcademicsPage() {
     return (
         <div className="min-h-screen flex flex-col items-center">
-            <HeroImage />
+            <HeroImage image={HeroImageFile} />
             <ProgramBlock />
             <ResourceBlock />
         </div>
