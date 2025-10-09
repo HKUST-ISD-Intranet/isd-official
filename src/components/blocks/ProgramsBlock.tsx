b27e18de74809c7c6aec0608b1dc8a56a3ec2486
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-09 17:07:31 +0800

    style(alignment): remove px for container to align components with navbar

--- src/components/blocks/ProgramsBlock.tsx
+++ src/components/blocks/ProgramsBlock.tsx
@@ -29,7 +29,7 @@ export default function ProgramBlock() {
 
     return (
         <div>
-            <div className="container relative flex flex-col py-section-gap gap-section-title-gap dot-pattern before:top-[-95px] before:right-[-90px] [--dot-color:var(--isd-secondary-1)]">
+            <div className="container relative flex flex-col py-section-gap gap-section-title-gap dot-pattern dot-pattern before:top-[-95px] before:right-0 [--dot-color:var(--isd-secondary-1)]">
                 <h1 className="text-h1 offset-text-background uppercase">
                     Programs
                 </h1>
