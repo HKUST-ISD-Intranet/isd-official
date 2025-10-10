7bc3354d724241b0292b6e7099d93014cd557d3b
Chan Chuen Wai (Candbot) <candbot@outlook.com>
2025-10-10 11:51:32 +0800

    feat/early-admission (#5)
    * feat: uploaded image
    
    * fix: well well well
    
    * feat: HeroImage can be used for any image
    
    * feat: early admission block
    
    * feat: page

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,11 +1,10 @@
-import HeroImg from '@/assets/academics-hero.jpg';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
-export default function BannerImage() {
+export default function BannerImage({ image }: { image: StaticImageData }) {
     return (
         <div className="w-full relative overflow-hidden h-[300px] md:h-[400px] lg:h-[612px]">
             <Image
-                src={HeroImg}
+                src={image}
                 alt={`Hero Banner Image`}
                 className={`object-cover w-full h-full`}
             />
