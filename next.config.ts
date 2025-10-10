f406cb7cd9015bb9b01fe3e82d97c4867d012b3e
Candbot Chan <candbot@outlook.com>
2025-10-10 10:42:12 +0800

    fix(next.config): fixed a runtime error by changing svg handling in webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,28 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export', // Enables static export
-    basePath: process.env.CI === 'true' ? '/isd-official' : '', 
-    assetPrefix: process.env.CI === 'true' ? '/isd-official/' : '', 
+    basePath: process.env.CI === 'true' ? '/isd-official' : '',
+    assetPrefix: process.env.CI === 'true' ? '/isd-official/' : '',
     trailingSlash: true,
     images: {
         unoptimized: true, // Disables image optimization for static export
     },
     webpack(config) {
         config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
         });
         return config;
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.ts', // Or "*.js"
+                },
+            },
+        },
     },
 };
 
