d04239150fab98055923fca173b3a6e786b87fc3
Candbot Chan <candbot@outlook.com>
2025-10-11 11:26:29 +0800

    feat: components

---
+++ src/components/blocks/WhatIsISDBlock.tsx
@@ -0,0 +1,78 @@
+export default function WhatIsISDBlock() {
+    return (
+        <div
+            className="container overflow-clip flex flex-col pt-section-gap gap-section-title-gap scroll-mt-[95px]"
+            id="what-is-isd"
+        >
+            <h1 className="text-h1 offset-text-background uppercase">
+                WHAT IS ISD
+            </h1>
+
+            <div className="flex flex-col gap-component-gap-sm">
+                <p className="text-md text-isd-font-1 text-justify">
+                    Integrative Systems and Design is a new paradigm for
+                    educating technologists and innovators. A more holistic
+                    systems perspective is necessary to truly innovate and
+                    tackle the problems of an ever-changing world. ISD uses
+                    integrative systems ranging from robotics to smart cities to
+                    biomedical systems to advance learning and knowledge through
+                    active teaching and transdisciplinary research. Embedding
+                    design and system thinking into the curriculum, combined
+                    with an emerging pedagogy of project-based active learning,
+                    will develop students’ technical competency, design
+                    thinking, understanding of complex and multi-disciplinary
+                    systems, and entrepreneurial spirit.
+                </p>
+
+                <div className="flex flex-col gap-[24px] border-l-3 border-isd-primary px-component-gap-sm py-element-gap bg-isd-primary-2 p-element-gap">
+                    <h2 className="text-h2 text-isd-primary">ISD Mission</h2>
+                    <ul>
+                        <li className="text-md text-isd-font-1 text-justify">
+                            <span className="italic font-bold text-isd-primary pr-[12px]">
+                                To
+                            </span>
+                            advance knowledge through active and differentiated
+                            learning, and transdisciplinary research in
+                            integrative systems design
+                        </li>
+                        <li className="text-md text-isd-font-1 text-justify">
+                            <span className="italic font-bold text-isd-primary pr-[12px]">
+                                To
+                            </span>
+                            provide a platform for students, especially those
+                            with unique talents and learning potentials, to
+                            contribute fully to society and the world
+                        </li>
+                        <li className="text-md text-isd-font-1 text-justify">
+                            <span className="italic font-bold text-isd-primary pr-[12px]">
+                                To
+                            </span>
+                            nurture innovators and researchers with design and
+                            system thinking mindset and entrepreneur spirit
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex flex-col gap-[24px] border-l-3 border-isd-secondary px-component-gap-sm py-element-gap bg-isd-secondary-1 p-element-gap">
+                    <h2 className="text-h2 text-isd-secondary">ISD Vision</h2>
+                    <ul>
+                        <li className="text-md text-isd-font-1 text-justify">
+                            <span className="italic font-bold text-isd-secondary pr-[12px]">
+                                To
+                            </span>
+                            be a Division that epitomizes the use of
+                            experiential learning in systems design
+                        </li>
+                        <li className="text-md text-isd-font-1 text-justify">
+                            <span className="italic font-bold text-isd-secondary pr-[12px]">
+                                To
+                            </span>
+                            be a cradle for nurture of innovators and
+                            researchers with design and system thinking mindset
+                            and entrepreneur spirit
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
