1746af9099ad0b9b7c5f7c02163830eb506c1941
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-11 15:10:49 +0800

    feat(research): align dot pattern to page instead of container

--- src/app/research/HeadingBlock.tsx
+++ src/app/research/HeadingBlock.tsx
@@ -1,7 +1,7 @@
 export default function HeadingBlock() {
     return (
-        <div className="container overflow-clip flex flex-col pt-section-gap gap-section-title-gap dot-pattern before:top-[-95px] before:right-0 [--dot-color:var(--isd-primary-2)]">
-            <h1 className="text-h1 offset-text-background uppercase">
+        <div className="w-full overflow-clip pt-section-gap gap-section-title-gap dot-pattern before:top-[-95px] before:right-[-10px] [--dot-color:var(--isd-primary-2)]">
+            <h1 className="text-h1 offset-text-background uppercase container">
                 Research Area
             </h1>
         </div>
