2284f8758bd56ca2b3582b9feb4a9c2e8fcf5c1b
Candbot Chan <candbot@outlook.com>
2025-10-11 09:55:55 +0800

    feat: route setup

---
+++ src/app/about-isd/page.tsx
@@ -0,0 +1,9 @@
+import HeroCarouselBlock from '@/components/blocks/HeroCarouselBlock';
+
+export default function AboutISD() {
+    return (
+        <div className="min-h-screen flex flex-col items-center">
+            <HeroCarouselBlock />
+        </div>
+    );
+}
