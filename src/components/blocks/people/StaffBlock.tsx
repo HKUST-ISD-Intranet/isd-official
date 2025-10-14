7d8952ad467fefaf2d88185a82538f67fd0e7d16
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-15 01:45:53 +0800

    feat(staff-block): fill dynamic data for staff

--- src/components/blocks/people/StaffBlock.tsx
+++ src/components/blocks/people/StaffBlock.tsx
@@ -1,18 +1,28 @@
 import StaffCard from '@/components/StaffCard';
+import staff from '@/data/staff.json';
 
 export default function StaffBlock() {
     return (
-        <div className="container w-full flex flex-col py-section-gap gap-section-title-gap">
+        <div
+            className="container w-full flex flex-col py-section-gap gap-section-title-gap"
+            id="staff"
+        >
             <h1 className="text-h1 offset-text-background uppercase">
                 ISD Staff
             </h1>
 
             <div className="grid grid-cols-3 gap-y-component-gap-sm gap-x-component-gap">
-                <StaffCard className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary" />
-                <StaffCard className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary" />
-                <StaffCard className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary" />
-                <StaffCard className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary" />
-                <StaffCard className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary" />
+                {staff.map((member, i) => (
+                    <StaffCard
+                        key={i}
+                        className="odd:border-isd-primary odd:bg-isd-primary-2 odd:text-isd-primary even:border-isd-secondary even:bg-isd-secondary-1 even:text-isd-secondary"
+                        name={member.name}
+                        role={member.role || undefined}
+                        email={member.email || undefined}
+                        phone={member.phone || undefined}
+                        location={member.location || undefined}
+                    />
+                ))}
             </div>
         </div>
     );
