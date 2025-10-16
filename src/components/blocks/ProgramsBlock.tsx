f3510d961ded0b7fd9314dd9e334c754128f992a
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-17 01:32:57 +0800

    fix(academics): fix dot pattern to position

--- src/components/blocks/ProgramsBlock.tsx
+++ src/components/blocks/ProgramsBlock.tsx
@@ -35,7 +35,7 @@ export default function ProgramBlock() {
 
     return (
         <div>
-            <div className="container relative overflow-clip flex flex-col py-section-gap gap-section-title-gap dot-pattern before:top-[-95px] before:right-0 [--dot-color:var(--isd-secondary-1)]">
+            <div className="container relative overflow-clip flex flex-col py-section-gap gap-section-title-gap">
                 <h1 className="text-h1 offset-text-background uppercase">
                     Program
                 </h1>
