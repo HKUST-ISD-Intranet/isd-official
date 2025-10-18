98c7cb9b497c4575f724164384f66845acb63988
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-18 12:03:11 +0800

    fix: typo

--- src/components/blocks/mphil-tie/ProgramContactBlock.tsx
+++ src/components/blocks/mphil-tie/ProgramContactBlock.tsx
@@ -2,7 +2,7 @@ export default function ProgramContactBlock() {
     return (
         <div className="flex flex-col items-start gap-[24px]">
             <h2 className="text-[36px] leading-[36px] font-bold text-isd-primary">
-                Application and Financial Assistance
+                Program Contact
             </h2>
             <p>
                 For inquiries, please contact the Program Office{' '}
